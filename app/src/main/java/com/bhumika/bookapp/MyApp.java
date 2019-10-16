9f2773bf8fa7f48e86b6a7f1afa33f64f6f0cfcb
Bhumika Techlabs <bhumika.techlabs@gmail.com>
2019-10-17 00:10:00 +0530

    Update

--- app/src/main/java/com/bhumika/bookapp/MyApp.java
+++ app/src/main/java/com/bhumika/bookapp/MyApp.java
@@ -4,10 +4,6 @@ import android.app.Application;
 
 import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * Created by Akshay on 31-12-2017.
- */
-
 public class MyApp extends Application {
 
     public static int flag=0;
